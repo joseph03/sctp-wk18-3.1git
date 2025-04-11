1e21770781b5e5bbc3befdb4d722b89f4063bdef
joseph03 <joseph03sg@gmail.com>
2025-04-12 03:38:13 +0800

    tfvars

--- local-public-ip.tf
+++ local-public-ip.tf
@@ -1,20 +1,5 @@
-data "http" "my_ip" {
-  url = "https://api.ipify.org?format=text"
-}
-
-# variable is used instead of local because
-# when terraform is run in my machine, then the value is
-# my local machine's public ip 
-# if terraform is run by github workflow, then the value is 
-# github's public ip
-# used this in security group of public ec2
-# That allows my laptop to SSH into all 3 EC2s — because they all share this security group.
+# variables.tf
 variable "my_ip_cidr" {
   type        = string
   description = "Public IP address of the machine running Terraform"
-  default     = "${chomp(data.http.my_ip.body)}/32"
 }
-#locals {
-#  my_ip_cidr = "${chomp(data.http.my_ip.body)}/32"   
-#}
-
